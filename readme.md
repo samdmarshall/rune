3d04e278fa0ea22e6c7cdf6d58872397765ed4a5
Samantha Marshall <me@samdmarshall.com>
2017-01-25 14:12:30 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# build
+
+`nim compile --out:secure-env --app:console --passL:"-framework Foundation -framework Security" secureEnv.nim`
+
+# usage
+this was primarily made for my own usage, but it unlocks and retrieves items out of a keychain that is registered with the system. I store a bunch of API tokens for my shell in a shared keychain so it is always in sync (and retrieves the password out of my login keychain to unlock). this acts as a wrapper around making those calls instead of using shell scripting when setting up a new shell session (considerably slower).
---
+++ secureEnv.nim
@@ -0,0 +1,54 @@
+# imports from standard library
+##
+import os
+import parseopt2
+
+# mark that we want to compile the companion "keychain.m" file as part of building this file.
+# additionally, declare the interface that should be used to access the bridged code to run the 
+# code that accesses the system APIs for the keychain
+##
+{.compile: "keychain.m".}
+proc getTokenFromKeychain(token_name: cstring, keychain_path: cstring): cstring {.importc.}
+
+# define the usage for "--help"
+##
+proc usage =
+  echo "usage: " & os.extractFilename(os.getAppFilename()) & " [--help|-h] [-v|--version] [-p|--keychain]:<keychain path> [-n|--name]:<item name>"
+
+# define the version number
+##
+proc version_info =
+  echo "secure-env v0.1"
+
+# ===========================================
+# this is the entry-point, there is no main()
+# ===========================================
+
+# define default values for the keychain path and name of the token
+var keychain_path: string = ""
+var secure_item_name: string = ""
+
+# iterate over the command line flags passed to the executable
+for kind, key, value in getopt():
+  case kind
+  of cmdLongOption, cmdShortOption:
+    case key
+    of "help", "h":
+      usage()
+    of "version", "v":
+      version_info()
+    of "keychain", "p":
+      var expanded_path: string = os.expandTilde(value)
+      keychain_path = os.expandFilename(expanded_path)
+    of "name", "n":
+      secure_item_name = value
+    else: discard
+  else: discard
+  
+
+# if the path specified by the keychain parameter exists and a string is given as the token name, then get it from the keychain.
+##
+if os.fileExists(keychain_path) and secure_item_name != "":
+  let token_value = getTokenFromKeychain(secure_item_name, keychain_path)
+  if token_value.len > 0:
+    echo(token_value)
